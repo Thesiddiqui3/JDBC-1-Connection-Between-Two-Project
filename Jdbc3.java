f2e79263f7a3ccd856983a6e72814cb4fcc90bc0
MOHD SHAMI <96070289+Thesiddiqui3@users.noreply.github.com>
2022-12-23 21:27:22 +0530

    Add files via upload
    in this project there is a method which is present in project JDBC3 and i am calling that method in project JDBC4

---
+++ Jdbc3.java
@@ -0,0 +1,7 @@
+package program1;
+
+public class Jdbc3 {
+	public static int  add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ Jdbc4.java
@@ -0,0 +1,13 @@
+3package program2;
+
+import program1.Jdbc3;
+
+public class Jdbc4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=Jdbc3.add(10,20);
+		System.out.println(sum);
+	}
+
+}
